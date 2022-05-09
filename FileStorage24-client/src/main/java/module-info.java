4c02abdbb1299351df12fad5e5de422262cbb8e3
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-05-10 00:55:33 +0300

    Initial commit

--- FileStorage24-client/src/main/java/module-info.java
+++ FileStorage24-client/src/main/java/module-info.java
@@ -1,9 +1,10 @@
 module ru.gb {
     requires javafx.controls;
     requires javafx.fxml;
-requires io.netty.all;
-requires io.netty.codec;
+    requires io.netty.all;
+    requires io.netty.codec;
     requires lombok;
+    requires FileStorage24.commons;
     opens ru.gb to javafx.fxml;
-    exports ru.gb;
+    exports ru.gb.client;
 }
---
+++ FileStorage24-commons/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module FileStorage24.commons {
+    requires lombok;
+    opens commons.commands;
+    exports ru.gb.commands;
+}
