31f2e343878135b84944cdb3218a7b67d1455a16
EvgeniyKurlyanchik <91161381+EvgeniyKurlyanchik@users.noreply.github.com>
2022-05-10 12:49:39 +0300

    Initial commit

---
+++ FileStorage24-commons/src/main/java/ru/gb/commands/FocusResponse.java
@@ -0,0 +1,17 @@
+package ru.gb.commands;
+
+import lombok.Getter;
+
+@Getter
+public class FocusResponse extends AbstractCommand{
+    private final String fileName;
+
+    public FocusResponse(String fileName) {
+        this.fileName = fileName;
+    }
+
+    @Override
+    public CommandType getType() {
+        return CommandType.FOCUS_RESPONSE;
+    }
+}
